a2e93810fce917e242d3d9bbeb6c3bdba76d2ad6
Sumit Chaudhary <digitalcodecorporation@gmail.com>
2017-06-18 10:39:28 +0530

    Commit From Ubuntu : Note App Done.
    This was only for learning purpose and to use yargs.

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const yargs = require('yargs');
 const notes = require('./notes');
-
-var command = yargs.argv['_'][0];
+const titleOptions = {
+        describe : 'Title of note',
+        demand : true,
+        alias : 't'
+    }
+    
+var command = yargs
+.command('add','Add a new note.',{
+    title : titleOptions,
+    body : {
+        describe : 'Body of note',
+        demand : true,
+        alias : 'b'
+    }
+})
+.command('list','List all notes.')
+.command('read','Get note by title.',{
+    title : titleOptions
+})
+.command('remove','Remove a note',{
+    title : titleOptions
+})
+.help().argv['_'][0];
 
 //console.log('Yargs commands :',yargs.argv);
 
@@ -12,16 +33,24 @@ if(command === 'add'){
     //console.log(title,note);
     notes.addNote(title,note);
 }else if(command === 'list'){
-    console.log('Listing all notes.');
     notes.listAll();
 }else if(command === 'read'){
     console.log('Reading note.');
     let title = yargs.argv.hasOwnProperty('title')?yargs.argv.title:'';
-    notes.readNote(title);
+    var note = notes.readNote(title);
+    console.log(note);
+    if(note){
+        //note = JSON.parse(note);
+        console.log('** Note found **');
+        console.log('Title :'+note.title);
+        console.log('Body :'+note.body);
+    }else{
+        console.log('Note not found.');
+    }
 }else if(command === 'remove'){
     console.log('Removing note.');
     let title = yargs.argv.hasOwnProperty('title')?yargs.argv.title:'';
-    notes.removeNote(title);
+    notes.removeNote(title)?('Deleting note :'+title):('Error in deleting note,no note exists with title :'+title);
 }else{
-    console.log('Command not recognized.');
+    console.log('Please provide command or use --help');
 }
--- notes.js
+++ notes.js
@@ -1,20 +1,59 @@
-const notes = [];
-var addNote = function (title, note) {
-    //console.log('Adding Note.');
-    notes[title] = note;
-    console.log(notes);
-    return true;
+const fs = require('fs');
+var fetchNotes = () => {
+    //Initializing notes array try..catch used to check if file exists.
+    try{
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    }catch(e){
+        return [];
+    }
+}
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+}
+var addNote = function (title, body) {
+    //Notes Array
+    var notes = fetchNotes();
+    
+    // Note to be added.
+    var note = {
+        title,
+        body
+    }
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    if(duplicateNotes.length == 0){
+        notes.push(note);
+        saveNotes(notes);
+    }else{
+        console.log('Trying to add duplicate Note.');
+    }
 }
 var readNote = function (title) {
-    console.log(notes[title]);
+    var notes = fetchNotes();
+    // console.log('Title to search :'+title);
+    var noteToSearch = undefined;
+    // Remember we can never return from forEach loop.
+    notes.forEach((note) => {
+        if(note.title === title){
+            // console.log('Title we got:'+JSON.stringify(note));
+            noteToSearch = note;
+        }
+            
+    });
+    return noteToSearch;
 }
 var removeNote = function(title){
-    delete notes[title]
-    return true;
+    var notes = fetchNotes();
+    var newNotes = notes.filter((note)=> note.title !== title);
+    return notes.length !== newNotes.length;
 }
 var listAll = function(){
-    notes.forEach(function(item,index,array){
-        console.log(index,':',notes[index]);
+    var notes = fetchNotes();
+    console.log(`Printing ${notes.length} note(s).`);
+    notes.forEach(function(note){
+        console.log('Title :'+note.title);
+        console.log('Body :'+note.body);
+        console.log('--------');
     });
 }
 // If key and value are same then we can remove key.
